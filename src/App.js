b6449eb8b086f42fa0921487e1fd07d927b9875f
Daniel Salomon <danielsalomon@Daniels-MacBook-Pro-2.local>
2024-03-01 13:43:00 +0100

    Updated GA

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactGA from 'react-ga4';
 
 import Header from './components/Header'
 import AllResources from './components/AllResources'
@@ -7,6 +8,11 @@ import HelperPopUp from './components/HelperPopUp'
 
 
 function App() {
+
+  ReactGA.initialize(`${process.env.REACT_APP_GA_ID}`)
+  // ReactGA.pageview(window.location.pathname + window.location.search)
+  // ReactGA.send({ hitType: "pageview", page: window.location.pathname })
+
   return (
     <>
       <div className="container text-center">
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ReactGA from 'react-ga4';
 
-
-ReactGA.initialize(`${process.env.REACT_APP_GA_ID}`)
-ReactGA.pageview(window.location.pathname + window.location.search)
-ReactGA.send({ hitType: "pageview", page: window.location.pathname })
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
