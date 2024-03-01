ba54b5826176ffaae724004943d182d514ec0370
Daniel Salomon <danielsalomon@Daniels-MacBook-Pro-2.local>
2024-03-01 13:39:09 +0100

    Updated GA

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,6 +7,7 @@ import ReactGA from 'react-ga4';
 
 ReactGA.initialize(`${process.env.REACT_APP_GA_ID}`)
 ReactGA.pageview(window.location.pathname + window.location.search)
+ReactGA.send({ hitType: "pageview", page: window.location.pathname })
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
