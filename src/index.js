dc1f60bf5090bfc950372383f0fa8a7854a74cb7
Daniel Salomon <danielsalomon@Daniels-MacBook-Pro-2.local>
2024-03-01 13:30:13 +0100

    Updated GA

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import ReactGA from 'react-ga4';
 
 
 ReactGA.initialize(`${process.env.REACT_APP_GA_ID}`)
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.pageview(window.location.pathname + window.location.search)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
